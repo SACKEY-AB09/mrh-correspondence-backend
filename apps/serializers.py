1dc746bd7f53058563115e55499462992b7e9e23
sackey <sackeyabraham67@gmail.com>
2026-08-01 00:41:05 +0000

    removed mistakenly restored files

--- apps/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class DateRangeQuerySerializer(serializers.Serializer):
-    start = serializers.DateField(required=False)
-    end = serializers.DateField(required=False)
--- correspondence_gh/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CorrespondenceGhConfig(AppConfig):
-    name = 'correspondence_gh'
