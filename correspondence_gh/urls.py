e4b9b1f97c79c5949879f10b4809d8335828b259
sackey <sackeyabraham67@gmail.com>
2026-08-14 12:53:11 +0000

    Fix media files returning 404 in production by always serving them, not just in DEBUG mode

--- correspondence_gh/urls.py
+++ correspondence_gh/urls.py
@@ -40,5 +40,5 @@ urlpatterns = [
     path("api/users/", CreateUserView.as_view(), name="create-user"),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
